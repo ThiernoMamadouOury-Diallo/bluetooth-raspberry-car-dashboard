e8f9b10c94319efd4bf52d7ce61f8730a3bac59e
alpha.barry <alpha-ibrahima1.barry@etu.u-pec.fr>
2019-05-24 14:03:21 +0200

    Add forgotten package

---
+++ services/carDashboardService.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+class CarDashboardService:
+    
+   def __init__(self):
+       # init service
+
+    def getFuelLevel(self):
+        return "Niveau du carburant est de 15 Litre"
+
+    
+    def getEngineTemperature(self):
+        return "La temperature du moteur est de 30 ° Celsius"
